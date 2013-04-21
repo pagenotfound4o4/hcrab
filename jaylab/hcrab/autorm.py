0d917deedace174ba05ed13e4cb63a616324c740
xiaojay <xiaojay@gmail.com>
2013-04-21 22:48:49 +0800

    fix bug

--- jaylab/hcrab/autorm.py
+++ jaylab/hcrab/autorm.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-import os, datetime
+import os, datetime, time
 from dateutil.relativedelta import *
 os.environ['DJANGO_SETTINGS_MODULE'] = 'jaylab.settings'
 from jaylab.hcrab.models import *
@@ -9,5 +9,7 @@ fs = VideoFile.objects.filter(latest_ref__lt=qt).all()
 for f in fs:
     try:
         os.remove(f.get_file_path())
+        time.sleep(0.1)
     except OSError:
         pass
+
